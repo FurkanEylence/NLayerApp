16fdcd0e725c56e1da0582077fd32d2aff2ea8c3
FurkanEylence <furkaneylence1@gmail.com>
2022-04-03 00:18:28 +0300

    After CoreLayer created

--- NLayer.Core/Concrete/Category.cs
+++ NLayer.Core/Concrete/Category.cs
@@ -9,6 +9,7 @@ namespace NLayer.Core.Concrete
 {
     public class Category : BaseEntity
     {
-
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
     }
 }
